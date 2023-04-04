f4638f95acdc6320d132ae71ab5c400698234ce0
devdewa <uchiasasuke123.sp@gmail.com>
2023-04-05 02:35:08 +0700

    Update responsive

--- app.vue
+++ app.vue
@@ -1,8 +1,4 @@
 <template>
-  <section class="bg-gray-800 min-h-screen px-3 lg:px-12 py-3 lg:py-12">
-    <div class="container mx-auto">
-      <NuxtPage />
-      <Footer />
-    </div>
-  </section>
+  <NuxtLayout>
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="bg-gray-800 min-h-screen px-3 lg:px-12 py-5 lg:py-12">
+    <div class="container mx-auto">
+      <Image />
+      <NuxtPage />
+      <Social />
+    </div>
+  </section>
+</template>
