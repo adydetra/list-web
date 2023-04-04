c1aca7db3cfda7bae7a590d4374f2b04ddd16a8b
devdewa <uchiasasuke123.sp@gmail.com>
2023-04-05 04:18:00 +0700

    Add PWA

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
+  <VitePwaManifest />
+  <NuxtLoadingIndicator />
   <NuxtLayout>
   </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,10 +12,51 @@ export default defineNuxtConfig({
 
   modules: [
     '@nuxtjs/tailwindcss',
-    'nuxt-headlessui'
+    'nuxt-headlessui',
+    '@vite-pwa/nuxt'
   ],
 
   headlessui: {
     prefix: 'Headless'
+  },
+
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: "Dewa Web",
+      short_name: "Dewa Web",
+      theme_color: '#0f172a',
+      background_color: '#0f172a',
+      icons: [
+        {
+          src: 'pwa/icon_64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa/icon_144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa/icon_192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa/icon_512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+      offline: true,
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+    },
   }
 })
