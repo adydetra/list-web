d6864f21675893692a62f6d3ff0e53ee56451baa
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-29 19:10:00 +0700

    style: formatting code

--- components/Social-Image.vue
+++ components/Social-Image.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container mx-auto flex justify-center gap-8 md:gap-14 lg:gap-28 items-center text-white lg:mb-12 py-12">
     <div class="space-x-8 lg:space-x-20">
-      <NuxtLink to="https://discord.gg/WzpazrK9NT" target="_blank"><Icon name="prime:discord" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8"/></NuxtLink>
-      <NuxtLink to="https://twitter.com/adityawarman_P" target="_blank"><Icon name="prime:twitter" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8"/></NuxtLink>
+      <NuxtLink to="https://discord.gg/WzpazrK9NT" target="_blank"><Icon name="prime:discord" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8" /></NuxtLink>
+      <NuxtLink to="https://twitter.com/adityawarman_P" target="_blank"><Icon name="prime:twitter" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8" /></NuxtLink>
     </div>
     <div>
-      <img src="/logo.png" alt="logo" class="w-20 lg:w-32 mx-auto rounded-full hover:rounded-3xl">
+      <img src="/logo.png" alt="logo" class="w-20 lg:w-32 mx-auto rounded-full hover:rounded-3xl" />
     </div>
     <div class="space-x-8 lg:space-x-20">
-      <NuxtLink to="https://github.com/devdewa" target="_blank"><Icon name="prime:github" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8"/></NuxtLink>
-      <NuxtLink to="https://www.linkedin.com/in/adityawarman-dewa-putra/" target="_blank"><Icon name="prime:linkedin" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8"/></NuxtLink>
+      <NuxtLink to="https://github.com/devdewa" target="_blank"><Icon name="prime:github" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8" /></NuxtLink>
+      <NuxtLink to="https://www.linkedin.com/in/adityawarman-dewa-putra/" target="_blank"
+        ><Icon name="prime:linkedin" class="cursor-pointer w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8"
+      /></NuxtLink>
     </div>
   </div>
 </template>
 
 <style>
-
 .cursor-pointer {
   @apply hover:-mt-1 hover:text-indigo-300 transition-all duration-100 ease-in-out;
 }
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,12 @@
   <div class="w-full max-w-6xl mx-auto px-2 sm:px-0">
     <TabGroup>
       <TabList class="flex space-x-3 lg:space-x-12 rounded-xl bg-blue-400/20 p-1">
-        <Tab
-          v-for="category in Object.keys(categories)"
-          as="template"
-          :key="category"
-          v-slot="{ selected }"
-        >
+        <Tab v-for="category in Object.keys(categories)" as="template" :key="category" v-slot="{ selected }">
           <button
             :class="[
               'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-sky-200',
               'ring-slate-200 ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-              selected
-                ? 'bg-slate-100 shadow text-sky-800'
-                : 'text-sky-200 hover:bg-slate-200/[0.12] hover:text-sky-200',
+              selected ? 'bg-slate-100 shadow text-sky-800' : 'text-sky-200 hover:bg-slate-200/[0.12] hover:text-sky-200',
             ]"
           >
             {{ category }}
@@ -26,53 +19,32 @@
         <TabPanel
           v-for="(posts, idx) in Object.values(categories)"
           :key="idx"
-          :class="[
-            'rounded-xl bg-slate-300 p-3',
-            'ring-slate-200 ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-          ]"
+          :class="['rounded-xl bg-slate-300 p-3', 'ring-slate-200 ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2']"
         >
           <ul>
-            <li
-              v-for="post in posts"
-              :key="post.id"
-              class="relative rounded-md p-3 hover:bg-gray-200"
-            >
+            <li v-for="post in posts" :key="post.id" class="relative rounded-md p-3 hover:bg-gray-200">
               <div class="flex justify-between items-center">
-                <h3 :class="['text-sm font-medium leading-5', post.class]">
-                  <Icon :name="post.icon" class="cursor-pointer w-3 h-3 -mt-0.5"/> {{ post.title }}
-                </h3>
+                <h3 :class="['text-sm font-medium leading-5', post.class]"><Icon :name="post.icon" class="cursor-pointer w-3 h-3 -mt-0.5" /> {{ post.title }}</h3>
                 <p class="hidden md:flex text-xs space-x-4 font-light text-gray-400">
-                  <Icon :name="post.mobile" class="cursor-pointer w-3.5 h-3.5 -mt-0.5"/>
-                  <Icon :name="post.desktop" class="cursor-pointer w-3.5 h-3.5 -mt-0.5"/>
-                  <Icon :name="post.webpwa" class="cursor-pointer w-3.5 h-3.5 -mt-0.5"/>
+                  <Icon :name="post.mobile" class="cursor-pointer w-3.5 h-3.5 -mt-0.5" />
+                  <Icon :name="post.desktop" class="cursor-pointer w-3.5 h-3.5 -mt-0.5" />
+                  <Icon :name="post.webpwa" class="cursor-pointer w-3.5 h-3.5 -mt-0.5" />
                 </p>
               </div>
 
-              <ul
-                class="mt-1 flex space-x-1 items-center text-[10px] md:text-xs font-normal leading-4 text-gray-500"
-              >
+              <ul class="mt-1 flex space-x-1 items-center text-[10px] md:text-xs font-normal leading-4 text-gray-500">
                 <li>{{ post.tech }}</li>
               </ul>
-              
-              <ul
-                class="md:hidden mt-4 md:mt-5 opacity-50 flex space-x-1 items-center text-xs font-normal leading-4 text-gray-500"
-              >
+
+              <ul class="md:hidden mt-4 md:mt-5 opacity-50 flex space-x-1 items-center text-xs font-normal leading-4 text-gray-500">
                 <li class="space-x-3">
-                  <Icon :name="post.mobile" class="cursor-pointer w-3 h-3 -mt-0.5"/>
-                  <Icon :name="post.desktop" class="cursor-pointer w-3 h-3 -mt-0.5"/>
-                  <Icon :name="post.webpwa" class="cursor-pointer w-3 h-3 -mt-0.5"/>
+                  <Icon :name="post.mobile" class="cursor-pointer w-3 h-3 -mt-0.5" />
+                  <Icon :name="post.desktop" class="cursor-pointer w-3 h-3 -mt-0.5" />
+                  <Icon :name="post.webpwa" class="cursor-pointer w-3 h-3 -mt-0.5" />
                 </li>
               </ul>
 
-              <NuxtLink
-                :to="post.link"
-                target="_blank"
-                :class="[
-                  'absolute inset-0 rounded-md',
-                  'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2',
-                ]"
-              >
-              </NuxtLink>
+              <NuxtLink :to="post.link" target="_blank" :class="['absolute inset-0 rounded-md', 'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2']"> </NuxtLink>
             </li>
           </ul>
         </TabPanel>
@@ -82,8 +54,8 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+import { ref } from "vue";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
 
 const categories = ref({
   Individual: [
@@ -168,7 +140,8 @@ const categories = ref({
       desktop: "ic:outline-desktop-windows",
       webpwa: "streamline:programming-web-server-world-internet-earth-www-globe-worldwide-web-network",
       icon: "vscode-icons:file-type-blade",
-    },{
+    },
+    {
       id: 2,
       title: "Averroes Unipessoal. Lda",
       link: "http://averroes.info",
@@ -201,5 +174,5 @@ const categories = ref({
       icon: "logos:codeigniter-icon",
     },
   ],
-})
+});
 </script>
