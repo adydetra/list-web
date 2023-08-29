ec6984e840ecd7317ab8e4a3d30f9269906e1d80
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-29 19:55:51 +0700

    chore: rename

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-slate-900 min-h-screen px-3 lg:px-12 pb-12">
     <div class="container mx-auto">
-      <Social-Image />
+      <social-img />
       <NuxtPage />
     </div>
   </section>
