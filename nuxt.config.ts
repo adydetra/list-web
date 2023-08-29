c50e267fd73509e7ef65cd5569d201988f1a54ff
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-29 19:10:32 +0700

    perf: update PWA workbox

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,77 +1,58 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Dewa Web',
+      title: "Dewa Web",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'More Website Dev Dewa' }
-      ]
-    }
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "More Website Dev Dewa" },
+      ],
+    },
   },
 
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-headlessui',
-    '@vite-pwa/nuxt',
-    'nuxt-icon'
-  ],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-headlessui", "nuxt-icon", "@vite-pwa/nuxt"],
 
   headlessui: {
-    prefix: 'Headless'
+    prefix: "Headless",
   },
 
   pwa: {
-    registerType: 'autoUpdate',
+    registerType: "autoUpdate",
     manifest: {
       name: "Dewa Web",
       short_name: "Dewa Web",
-      theme_color: '#0f172a',
-      background_color: '#0f172a',
+      theme_color: "#0f172a",
+      background_color: "#0f172a",
       icons: [
         {
-          src: 'pwa/icon_64x64.png',
-          sizes: '64x64',
-          type: 'image/png',
+          src: "pwa/icon_64x64.png",
+          sizes: "64x64",
+          type: "image/png",
         },
         {
-          src: 'pwa/icon_144x144.png',
-          sizes: '144x144',
-          type: 'image/png',
+          src: "pwa/icon_144x144.png",
+          sizes: "144x144",
+          type: "image/png",
         },
         {
-          src: 'pwa/icon_192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
+          src: "pwa/icon_192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          src: 'pwa/icon_512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
+          src: "pwa/icon_512x512.png",
+          sizes: "512x512",
+          type: "image/png",
         },
       ],
     },
     workbox: {
-      runtimeCaching: [
-        {
-          urlPattern: ({ url }) => {
-            return url.pathname.startsWith("/");
-          },
-          handler: "CacheFirst" as const,
-          options: {
-            cacheName: "api-cache",
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-      ],
-      // navigateFallback: '/',
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      navigateFallback: "/",
+      globPatterns: ["*"],
     },
     devOptions: {
       enabled: true,
-      type: 'module',
+      type: "module",
     },
-  }
-})
+  },
+});
