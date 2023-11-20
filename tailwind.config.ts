26a914ef2593cabd057f158d9a3681e77a3798ce
devdewa <uchiasasuke123.sp@gmail.com>
2023-11-20 16:17:46 +0700

    refactor: improved tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  content: ["./components/**/*.{vue,js}", "./layouts/**/*.vue", "./pages/**/*.vue", "./nuxt.config.{js,ts}", "./app.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
